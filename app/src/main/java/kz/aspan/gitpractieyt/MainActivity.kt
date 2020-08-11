248ca6071335d7fcc52173c1d1586877e07952b9
Aspannn <52127983+Aspannn@users.noreply.github.com>
2020-08-11 15:31:30 +0600

    Experimental Commit

--- app/src/main/java/kz/aspan/gitpractieyt/MainActivity.kt
+++ app/src/main/java/kz/aspan/gitpractieyt/MainActivity.kt
@@ -10,5 +10,7 @@ class MainActivity : AppCompatActivity() {
 
         println("THIS IS CONTAINED IN MY SECOND COMMIT")
         println("4 commit")
+        println("Local change")
+        println("Experimental")
     }
 }
